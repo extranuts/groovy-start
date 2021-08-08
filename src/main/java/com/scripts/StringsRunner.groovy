e133a5a1e08db97e76ff915648b56ccc16befc00
extranuts <QroWka8462>
2021-08-08 23:37:11 +0300

    Strings  def

---
+++ src/main/java/com/scripts/StringsRunner.groovy
@@ -0,0 +1,28 @@
+package com.scripts
+
+def name = "Ivan'''''''''''''\''''"
+def name2 = 'Ivan"""""""\""""'
+def name3 = '''Ivan ivanov'''
+def strValue = "Hello $name"
+println strValue
+
+value = """
+SELECT * 
+FROM table
+WHERE name = ${getWithPrefix(name)}
+"""
+println value
+
+def value5 = /Hello world $name3/
+
+def value4 = $/Hello word $name3/$
+
+println name[2]
+println name[-2]
+println name[1..5]
+println name * 3
+println name - 'an'
+
+def getWithPrefix(String name) {
+    "prefix-" + name
+}

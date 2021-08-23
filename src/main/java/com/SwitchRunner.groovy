7f520a213ff5a73f5908a144db524198b611d6e1
extranuts <QroWka8462>
2021-08-23 14:25:42 +0300

    Switch  Groovy

--- src/main/java/com/SwitchRunner.groovy
+++ src/main/java/com/SwitchRunner.groovy
@@ -12,18 +12,27 @@ class SwitchRunner {
             case 5:     // equals ==  ===
                 println 1
                 break
+            case new Person(29):
+                println 5
+                break
             case ~/\d+/: // pattern.matcher(12.toString()).matches()
                 println 2
                 break
             case { it % 5 == 0 }:   //Closure
                 println 3
                 break
-            case [1, 3, 5, 9]:    // Collection
+            case [1, 3, 5, 9]:    // list collection (not array) is case
                 println 4
                 break
             default:
                 println "None"
                 break
+        }
+
+        // list collection (not array) is case
+
+        if (x in [1, 3, 12]){
+            println x
         }
     }
 }

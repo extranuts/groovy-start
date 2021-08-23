96e75486f8c49f885cef1bda64321f348b6afcd3
extranuts <QroWka8462>
2021-08-23 14:09:37 +0300

    Conditions Groovy

--- src/main/java/com/ConditionsRunner.groovy
+++ src/main/java/com/ConditionsRunner.groovy
@@ -17,8 +17,24 @@ class ConditionsRunner {
     static void main(String[] args) {
         int x = 0
 //        def boolResult = x >10
-        if(x) {
+        if (x) {
             println x
         }
+
+        def person = new Person(19)
+        if (person) {
+            println person.getId()
+        }
+
+//        if(person != null){
+//            person.getId()
+//        }
+//        NOT NULL ?
+//        person?.getId()
+
+
+//        def result = x ? x : 1
+        def result = x ?: 1
+        println result
     }
 }

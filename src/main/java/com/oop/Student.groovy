a8a0add335f8940b08172541006e323b96e87c4e
extranuts <QroWka8462>
2021-08-25 13:22:31 +0300

    Learn groovy transforms (annotations AST)

--- src/main/java/com/oop/Student.groovy
+++ src/main/java/com/oop/Student.groovy
@@ -1,11 +1,22 @@
 package com.oop
 
+import groovy.transform.Canonical
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.Immutable
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
+import groovy.transform.builder.Builder
+import groovy.util.logging.Slf4j
 
 
 @ToString
 @TupleConstructor
+@EqualsAndHashCode
+//@Canonical
+@Immutable
+@Builder
+//@Slf4j
+//@Mixin
 class Student implements WithId {
     String firstName
     String lastName
@@ -13,5 +24,13 @@ class Student implements WithId {
 
     def getAt(Integer index) {
         index == 0 ? firstName: lastName
+    }
+
+    static void main(String[] args) {
+//        Student.builder()
+//        .lastName("Ivanov")
+//        .firstName("Ivan")
+//        .age(18)
+//        .build()
     }
 }
